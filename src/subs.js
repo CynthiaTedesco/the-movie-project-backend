a89d5c2830bfdb2ba91c58c8e7693e8cb19e56a0
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2019-10-31 10:57:57 +0100

    First srt file downloaded from subscene

--- src/subs.js
+++ src/subs.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
-const isZip = require('is-zip');
-const puppeteer = require('puppeteer');
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
-const JSZip = require("jszip");
 const fs = require("fs");
-// import * as fs from 'fs-zip'
-const Blob = require('blob');
-import  {saveAs} from 'file-saver';
+
+const iconvLite = require('iconv-lite');
+const chardet = require('chardet');
+
+const path = require('path');
+const tmp = require('tmp');
+const download = require('download');
+const rimraf = require('rimraf');
+const glob = require('glob');
 
 const baseURL = 'https://subscene.com';
 const searchByTitle = '/subtitles/searchbytitle';
@@ -27,10 +30,7 @@ const languages = {
 async function execute() {
     console.log('==========================================================================');
 
-    const itis = isZip(fs.readFileSync('./subtitles/the_lion_king.zip'));
-    console.log(itis);
-
-    //de- hard code
+    //TODO de- hard code
     const title = 'the lion king';
     const year = '2019';
     const language = languages.en.code;
@@ -39,124 +39,16 @@ async function execute() {
     const subtitlesListLink = await findSubtitlesListPath(path, language);
     const subtitleDownloadPath = await findSubtitleDownloadPath(subtitlesListLink, language);
 
-    // const subtitleDownloadPath = '/subtitles/english-text/2jJpEdBIFGwJWyPLS-0j2TRzikFypVe4Mw9R8Re4VH-G2C6jDDFPHr5yHLAcRXBXQSSUgffdJbediTc4drMZdRr1DUZ2A-CLJX2mMFuT5AKX4PrxVuWMWtVsJ1feVvA-0';
+    const url = baseURL + subtitleDownloadPath;
 
-    const url = baseURL+subtitleDownloadPath;
-
-    axios.get(url, {
-        withCredentials: true,
-        headers:{
-            'cookie': 'LanguageFilter=' + language,
-            'accept-encoding': 'gzip, deflate, br',
-            // 'data-type': 'text',
-            // 'mime-type': 'text/plain; charset=x-user-defined',
-        }
-    }).then(results =>{
-        const fileName = title.split(' ').join('_');
-        // const path2 = './subtitles/'+fileName+'_.zip';
-
-        saveZipFile('./subtitles/'+fileName+'_.zip', results);
-        // downloadFile('./subtitles/'+fileName+'.zip', results.data);
-    });
+    const movieName = title.split(' ').join('_');
+    downloadSubtitle(url, movieName);
 }
 
 execute();
 
-async function saveZipFile(path, results){
-    //TODO try to save regular zip file first. (with some of those libraries) https://davidwalsh.name/javascript-zip
-    //then continue
-
-    //https://blog.logrocket.com/binary-data-in-the-browser-untangling-an-encoding-mess-with-javascript-typed-arrays-119673c0f1fe/
-    //https://github.com/eligrey/FileSaver.js/issues/156
-
-    let newContent = "";                                    //solution code
-    for (var i = 0; i < results.data.length; i++) {         //solution code
-        newContent += String.fromCharCode(results.data.charCodeAt(i) & 0xFF); //solution code
-    }
-    var bytes = new Uint8Array(newContent.length);                     //modified
-    for (var i=0; i<newContent.length; i++) {                          //modified
-        bytes[i] = newContent.charCodeAt(i);                           //modified
-    }
-
-    const zip = new JSZip();
-    await zip.loadAsync(results.data, {type:'blob'});
-    zip.generateAsync({type: "uint8array"}).then(function (content) {
-        fs.writeFile(path2, content, function(err){
-            const itis = isZip(fs.readFileSync(path2));
-            console.log(err, itis);
-        });
-    });
-
-
-
-
-
-
-    // let newContent = "";                                //solution code
-    // for (var i = 0; i < results.data.length; i++) {         //solution code
-    //     newContent += String.fromCharCode(results.data.charCodeAt(i) & 0xFF); //solution code
-    // }
-    // var bytes = new Uint8Array(newContent.length);                     //modified
-    // for (var i=0; i<newContent.length; i++) {                          //modified
-    //     bytes[i] = newContent.charCodeAt(i);                           //modified
-    // }
-    //
-    // const buff = Buffer.from(bytes);
-    // fs.writeFile('./subtitles/'+fileName+'.zip', buff, function(err){
-    //     const itis = isZip(fs.readFileSync('the_lion_king.zip'));
-    //     console.log(err);
-    // });
-
-
-    // const blob = new Blob([bytes], {type: "application/zip"});
-    // saveAs(blob, './subtitles/'+fileName+'.zip');
-
-
-
-    // zip.load(newContent);                                              //modified
-    // jsonData = JSON.parse(zip.file('shouldBeThere.json').asText());
-
-
-    //     //https://github.com/GoogleChrome/puppeteer/issues/299 !!!!!! pauldraper commented on Mar 19 •
-    //
-    //     console.log('done!');
-}
-async function downloadFile(filename, content){
-    // fs.writeFile(filename, content, (e) => {
-    //     console.log(e);
-    // });
-
-                // const vDom = new JSDOM('<div></div>');
-                //
-                // if (typeof vDom.window.URL.createObjectURL === 'undefined') {
-                //     Object.defineProperty(vDom.window.URL, 'createObjectURL', { value: ()=>{}})
-                // }
-                //
-                // const url = vDom.window.URL.createObjectURL(content);
-                // const link = vDom.window.document.createElement('a');
-                // link.href = url;
-                // link.setAttribute('download', filename);
-                // vDom.window.document.body.appendChild(link);
-                // link.click();
-                // vDom.window.document.body.removeChild(link);
-
-    // const zip = new JSZip();
-    // await zip.loadAsync(content, {base64: true});
-    // const blob = await zip.generateAsync({type:"blob"});
-    //
-    // const vDom = new JSDOM('<div></div>');
-    //
-    // const element = vDom.window.document.createElement("a");
-    // element.setAttribute("href", vDom.window.URL.createObjectURL(blob));
-    // element.setAttribute("download", filename);
-    // element.style.display = "none";
-    // vDom.window.document.body.appendChild(element);
-    // element.click();
-    // vDom.window.document.body.removeChild(element);
-}
-
-function findSubtitleDownloadPath(path, language){
-    const url = baseURL+path;
+function findSubtitleDownloadPath(path, language) {
+    const url = baseURL + path;
 
     return axios.get(url, {
         withCredentials: true,
@@ -169,8 +61,8 @@ function findSubtitleDownloadPath(path, language){
     });
 }
 
-function findSubtitlesListPath(path, language){
-    const url = baseURL+path;
+function findSubtitlesListPath(path, language) {
+    const url = baseURL + path;
 
     return axios.get(url, {
         withCredentials: true,
@@ -183,8 +75,8 @@ function findSubtitlesListPath(path, language){
     });
 }
 
-function findPath(title, year, language){
-    return axios.post(baseURL+searchByTitle, {query: title}, {
+function findPath(title, year, language) {
+    return axios.post(baseURL + searchByTitle, {query: title}, {
         withCredentials: true,
         headers: {
             'cookie': 'LanguageFilter=' + language
@@ -220,4 +112,119 @@ function findPath(title, year, language){
         }
 
     });
+}
+
+function downloadSubtitle(downloadLink, movieName) {
+    fetch(downloadLink)
+        .then(function (subtitle) {
+            // Convert to UTF8
+            subtitle.content = convertToUTF8(subtitle.content);
+            return subtitle;
+        }).then(function (subtitle) {
+            // Save the subtitle to a file with a matching name
+            return saveSubtitle(subtitle.content, subtitle.type, movieName);
+        }).then(function () {
+            // Done
+            console.log('Done !');
+
+        }).catch(function (error) {
+
+            // Something went wrong
+            if (error) {
+                console.error(error.toString());
+            }
+
+        });
+}
+
+/**
+ * Save Subtitle
+ *
+ * @param {String}   content
+ * @param {String}   type (str, ass)
+ * @param movieName
+ * @return {Promise}
+ */
+function saveSubtitle(content, type, movieName) {
+
+    return new Promise(function (resolve, reject) {
+        // Save the subtitle file
+        fs.writeFile('./subtitles/' + movieName + '.' + type, content, 'utf8', function (error) {
+
+            // Something went wrong
+            if (error) {
+                return reject(error);
+            }
+
+            resolve();
+
+        });
+    });
+
+}
+
+/**
+ * Convert encoding to UTF-8
+ *
+ * @param  {Buffer} buffer
+ * @return {String}
+ */
+function convertToUTF8(buffer) {
+    // Detect the encoding
+    var detectedEncoding = chardet.detect(buffer);
+
+    // Already UTF8
+    if (!detectedEncoding || detectedEncoding.toLowerCase() == 'utf-8' || detectedEncoding.toLowerCase() == 'ascii') {
+        return buffer.toString();
+    }
+
+    return iconvLite.decode(buffer, detectedEncoding);
+}
+
+/**
+ * Download, extract, and get the content of the subtitle file
+ *
+ * @param  {String}  downloadLink
+ * @return {Promise} resolve with {content: buffer, type: (srt, ass, sub)}
+ */
+function fetch(downloadLink) {
+
+    return new Promise(function (resolve, reject) {
+
+        var tmpDir = tmp.dirSync().name;
+
+        // Download and export the subtitle file
+        download(downloadLink, tmpDir, {extract: true}).then(function () {
+
+            // Get the subtitle file
+            glob(path.join(tmpDir, '*.+(srt|ass|sub)'), {}, function (error, files) {
+
+                // Something went wrong
+                if (error) {
+                    rimraf.sync(tmpDir);
+                    return reject(error);
+                }
+
+                // Subtitle file not found
+                if (!files.length) {
+                    rimraf.sync(tmpDir);
+                    return reject(new Error('Invalid subtitle file'));
+                }
+
+                resolve({
+                    // Read the subtitle file
+                    content: fs.readFileSync(files[0]),
+                    // Get the type of the subtitle file
+                    type: path.extname(files[0]).toLowerCase().substr(1)
+                });
+
+                // Cleanup
+                rimraf.sync(tmpDir);
+
+            });
+
+        });
+
+    });
+
 }
