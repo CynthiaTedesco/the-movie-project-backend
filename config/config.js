09293c9e0fabdc2d545e5bba4dfcee2441b7037f
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2020-01-30 22:27:19 +0100

    heroku tweaks

--- config/config.js
+++ config/config.js
@@ -1,28 +1,29 @@
-require('dotenv/config');
+require('dotenv/config')
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+  development: {
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // dialect: process.env.DB_DIALECT
+    // }
+    //  "test": {
+    username: 'postres',
+    password: root,
+    database: 'mp1',
+    host: 'localhost',
+    port: 5433,
+    dialect: 'postgres'
+    //    "operatorsAliases": false
   }
-//  "test": {
-//    "username": "root",
-//    "password": null,
-//    "database": "database_test",
-//    "host": "127.0.0.1",
-//    "dialect": "mysql",
-//    "operatorsAliases": false
-//  },
-//  "production": {
-//    "username": "root",
-//    "password": null,
-//    "database": "database_production",
-//    "host": "127.0.0.1",
-//    "dialect": "mysql",
-//    "operatorsAliases": false
-//  }
+  //  "production": {
+  //    "username": "root",
+  //    "password": null,
+  //    "database": "database_production",
+  //    "host": "127.0.0.1",
+  //    "dialect": "mysql",
+  //    "operatorsAliases": false
+  //  }
 }
