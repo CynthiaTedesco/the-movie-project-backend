0a35b773a5e79b7a37dbfc23813c434f43abb560
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2020-02-04 15:51:15 +0100

    Add a Procfile

--- config/config.js
+++ config/config.js
@@ -2,20 +2,20 @@ require('dotenv/config')
 
 module.exports = {
   development: {
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // dialect: process.env.DB_DIALECT
-    // }
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+    }
     //  "test": {
-    username: 'postres',
-    password: root,
-    database: 'mp1',
-    host: 'localhost',
-    port: 5433,
-    dialect: 'postgres'
+    // username: 'postres',
+    // password: root,
+    // database: 'mp1',
+    // host: 'localhost',
+    // port: 5433,
+    // dialect: 'postgres'
     //    "operatorsAliases": false
   }
   //  "production": {
