7dcc4321c54f3181a4f93ed65c0c121f2229bf79
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2020-02-16 20:12:10 +0100

    Update server

--- config/config.js
+++ config/config.js
@@ -7,7 +7,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    logging: false
   },
   test: {
     use_env_variable: "HEROKU_DB_URL",
