2839bf3ca0502396a6624aa2d885bd899b93ddb0
Cynthia Tedesco <cynthia.tedesco@gmail.com>
2020-03-15 23:49:58 +0100

    Login endpoint

--- src/auth.js
+++ src/auth.js
@@ -6,40 +6,42 @@ import * as jwt from 'jsonwebtoken'
 const bcrypt = require('bcrypt')
 const saltRounds = 10
 
-// function generateToken(user) {
-//   const data = {
-//     _id: user._id,
-//     username: user.username
-//   }
-//   const signature = 'MySuP3R_z3kr3t'
-//   const expiration = '6h'
+function generateToken(user) {
+  const data = {
+    _id: user._id,
+    username: user.username
+  }
+  const signature = 'MySuP3R_z3kr3t'
+  const expiration = '6h'
 
-//   return jwt.sign({ data }, signature, { expiresIn: expiration })
-// }
+  return jwt.sign({ data }, signature, { expiresIn: expiration })
+}
 
 export async function login(req, res) {
-  // const userRecord = await models['user'].findOne({ username })
-  // if (!userRecord) {
-  //   throw new Error('User not found')
-  // } else {
-  //   bcrypt.compare(req.body.password, userRecord.password, function(
-  //     err,
-  //     result
-  //   ) {
-  //     if (result == true) {
-  //       res.redirect('/home')
-  //     } else {
-  //       throw new Error('Incorrect password')
-  //       // res.send('Incorrect password')
-  //       // res.redirect('/')
-  //     }
-  //   })
-  // }
-  // return {
-  //   user: userRecord.username,
-  //   token: generateToken(userRecord)
-  // }
-  // // res.status(200).send('ALALLALALALAL')
+  const userRecord = await models['user'].findOne({
+    where: { username: req.body.username }
+  })
+  if (!userRecord) {
+    throw new Error('User not found')
+  } else {
+    bcrypt.compare(req.body.password, userRecord.password, function(
+      err,
+      result
+    ) {
+      if (result == true) {
+        return res.status(200).send({
+          user: userRecord.username,
+          token: generateToken(userRecord)
+        })
+      } else {
+        throw new Error('Incorrect password')
+      }
+    })
+  }
+  return {
+    user: userRecord.username,
+    token: generateToken(userRecord)
+  }
 }
 
 export async function signUp(req, res) {
